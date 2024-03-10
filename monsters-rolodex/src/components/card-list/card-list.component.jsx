4bf76869a39dfbba8500ffa24f9794502c2616bd
Nicholas Walton <ngwalton@gmail.com>
2024-03-10 14:25:29 -0500

    Add image and email

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -10,9 +10,16 @@ class CardList extends Component {
       <div className='card-list'>
         {
           // eslint-disable-next-line
-          monsters.map(monster => {
-            return <div key={monster.id}><h2>{monster.name}</h2></div>
-          })
+          monsters.map(monster => (
+            <div className='card-container' key={monster.id}>
+              <img
+                alt={`monster ${monster.name}`}
+                src={`https://robohash.org/${monster.id}/?set=set2&size=180x180`}
+              />
+              <h2>{monster.name}</h2>
+              <p>{monster.email}</p>
+            </div>
+          ))
         }
       </div>
     )
