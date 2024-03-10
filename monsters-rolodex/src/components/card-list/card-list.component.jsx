ba99c81c43638fe111d2066ed5fe1d3c47569ca6
Nicholas Walton <ngwalton@gmail.com>
2024-03-10 14:58:15 -0500

    Put card in its own component

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component, React } from 'react'
+import Card from '../card/card.component.jsx'
 import './card-list.styles.css'
 
 class CardList extends Component {
@@ -11,17 +12,8 @@ class CardList extends Component {
         {
           // eslint-disable-next-line
           monsters.map(monster => {
-            const { id, name, email } = monster
-
             return (
-              <div className='card-container' key={id}>
-                <img
-                  alt={`monster ${name}`}
-                  src={`https://robohash.org/${id}/?set=set2&size=180x180`}
-                />
-                <h2>{name}</h2>
-                <p>{email}</p>
-              </div>
+              <Card key='' monster={monster} />
             )
           })
         }
---
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -0,0 +1,22 @@
+import { Component, React } from 'react'
+import './card.styles.css'
+
+class Card extends Component {
+  render () {
+    // eslint-disable-next-line
+    const { id, email, name } = this.props.monster
+
+    return (
+      <div className='card-container' key={id}>
+        <img
+          alt={`monster ${name}`}
+          src={`https://robohash.org/${id}/?set=set2&size=180x180`}
+        />
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    )
+  }
+}
+
+export default Card
