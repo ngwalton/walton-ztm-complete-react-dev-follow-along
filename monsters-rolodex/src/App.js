81403afe0b3e680d3fd78946b35777b57de1a668
Nicholas Walton <ngwalton@gmail.com>
2024-03-09 23:07:52 -0600

    Print monsters to page

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component, React } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      monsters: [
+        {
+          name: 'Andy',
+          id: 'alksdfjae'
+        },
+        {
+          name: 'Sam',
+          id: 'a;lskdfjaie'
+        },
+        {
+          name: 'Bob',
+          id: 'a;lsdkfje'
+        }
+      ]
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => {
+            return <div key={monster.id}><h2>{monster.name}</h2></div>
+          })
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
