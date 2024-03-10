cec6cd0edb55451383518f3146ddd586b670cf60
Nicholas Walton <ngwalton@gmail.com>
2024-03-10 14:31:40 -0500

    Destructure

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -10,16 +10,20 @@ class CardList extends Component {
       <div className='card-list'>
         {
           // eslint-disable-next-line
-          monsters.map(monster => (
-            <div className='card-container' key={monster.id}>
-              <img
-                alt={`monster ${monster.name}`}
-                src={`https://robohash.org/${monster.id}/?set=set2&size=180x180`}
-              />
-              <h2>{monster.name}</h2>
-              <p>{monster.email}</p>
-            </div>
-          ))
+          monsters.map(monster => {
+            const { id, name, email } = monster
+
+            return (
+              <div className='card-container' key={id}>
+                <img
+                  alt={`monster ${name}`}
+                  src={`https://robohash.org/${id}/?set=set2&size=180x180`}
+                />
+                <h2>{name}</h2>
+                <p>{email}</p>
+              </div>
+            )
+          })
         }
       </div>
     )
