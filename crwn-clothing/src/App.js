49082cb223ce3b0189f552deb111ed5799c32d1c
Nicholas Walton <ngwalton@gmail.com>
2024-03-12 13:37:48 -0500

    Add basic structure

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,5 +1,41 @@
 function App() {
-  return <div>Hello world!</div>;
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: `Women's`,
+    },
+    {
+      id: 5,
+      title: `Men's`,
+    },
+  ];
+  return (
+    <div className="categories-container">
+      {categories.map(({ id, title }) => {
+        return (
+          <div key={id} className="category-container">
+            <div className="background-image" />
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop now</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
