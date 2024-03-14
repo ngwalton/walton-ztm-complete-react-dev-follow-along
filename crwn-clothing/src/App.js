c5991f7be3559699bb3167e5879532c4955db121
Nicholas Walton <ngwalton@gmail.com>
2024-03-14 11:16:21 -0500

    Move sign-in to authentication

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import SignIn from './routes/authentication/authentication.component';
 
 function Shop() {
   return <h2>Shop</h2>;
