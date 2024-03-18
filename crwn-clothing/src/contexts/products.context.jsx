c8f5e10f36b7d2bde61efd3e9dfbce011ee85aea
Nicholas Walton <ngwalton@gmail.com>
2024-03-18 14:00:46 -0500

    Upload and download category data
    Uploading code is commented out as it only needs to run once.

--- crwn-clothing/src/contexts/products.context.jsx
+++ crwn-clothing/src/contexts/products.context.jsx
@@ -1,6 +1,9 @@
-import { createContext, useState, useMemo } from 'react';
+import { createContext, useState, useMemo, useEffect } from 'react';
 
-import PRODUCTS from '../shop-data.json';
+// import { addCollectionAndDocument } from '../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+
+// import SHOP_DATA from '../shop-data';
 
 // actual value you want to access
 export const ProductsContext = createContext({
@@ -10,7 +13,23 @@ export const ProductsContext = createContext({
 // eslint-disable-next-line react/prop-types
 export function ProductProvider({ children }) {
   // eslint-disable-next-line no-unused-vars
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  // this isn't the place to be uploading data, but we did it anyway
+  // useEffect(
+  //   () => async () => addCollectionAndDocument('categories', SHOP_DATA),
+  //   []
+  // );
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+
+    getCategoriesMap();
+  }, []);
+
   const value = useMemo(() => ({ products }), [products]);
 
   return (
--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -9,7 +9,16 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  // writeBatch,
+  query,
+  getDocs,
+} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -35,6 +44,33 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 // export const signInWithGoogleRedirect = () =>
 //   signInWithRedirect(auth, provider);
 export const db = getFirestore();
+
+// export const addCollectionAndDocument = async (collectionKey, objectsToAdd) => {
+//   const collectionRef = collection(db, collectionKey); // get the collection
+//   const batch = writeBatch(db); // get a batch ref
+
+//   objectsToAdd.forEach((object) => {
+//     const docRef = doc(collectionRef, object.title.toLowerCase());
+//     batch.set(docRef, object); // att batch set call
+//   });
+
+//   await batch.commit(); // all or nothing transaction
+//   console.log('done');
+// };
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+
+  return categoryMap;
+};
 
 export const createUserDocumentFromAuth = async (
   userAuth,
